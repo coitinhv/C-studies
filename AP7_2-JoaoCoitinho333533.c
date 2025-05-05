1194638fad7a867d176a04f0d26764d1e8dda801
coitinhv <153555368+coitinhv@users.noreply.github.com>
2025-05-05 12:05:34 -0300

    Create AP7_2-JoaoCoitinho333533.c

---
+++ AP7_2-JoaoCoitinho333533.c
@@ -0,0 +1,65 @@
+//Uma banca online de venda de frutas vende tre s tipos de frutas: laranja, maça e lima o. Faça um programa
+//em que o cliente informa a quantidade de cada fruta que deseja comprar, o endereço para entrega e se o mesmo
+//se localiza na zona A ou B da cidade. O cliente sempre deve informar as quantidades das 3 frutas, caso ele na o
+//deseje comprar alguma delas deve entrar o valor zero
+
+//a) (1,5pts) Uma funcao que retorne o preço total da compra ao receber as quantidades desejadas (em
+//nu mero de frutas de cada tipo) e adicionando ao preço base o imposto de cada fruta segundo a lista:
+
+//qb) (1,5pts) Uma funça o que calcule o valor da entrega que deve considerar:
+//• Zona de entrega: se A a entrega e R$ 10,00 e se for B e R$ 5,00.
+//• Cada entrega somente pode levar 25 unidades, caso o cliente tenha adquirido mais de 25 vai pagar
+//mais entregas. Por exemplo: cliente selecionou 51 frutas vai pagar 3 entregas.
+//c) (1,5pts) Uma funcao sem retorno de valor que imprime o valor total das frutas, valor total da entrega e
+//valor total geral.
+//d) (1,5pts) No main do programa, apo s a entrada de dados utilize somente as funço es acima para calcular e
+//exibir o valor total a pagar.
+
+
+#include <stdio.h>
+
+#define QUANT_FRUTAS 3
+
+int calculaPreco(int laranja, int maca, int limao);
+
+int main(){
+    char fruteira[QUANT_FRUTAS][20] = {"laranja", "maca", "limao"};
+    char endereco[100] = {};
+    int frutas[QUANT_FRUTAS] = {};
+    int fruta = 0, preco = 0;
+    char area;
+    printf("Tabela de precos:\nLaranja: R$3,00 | 2%% imposto\nMaca: R$2,00 | 5%% imposto\nLimao: R$1,00 | 3%% imposto");
+
+    for (int i = 0; i < QUANT_FRUTAS;i++){
+        printf("\nPor favor, informe a quantidade desejada de %ss: ", fruteira[i]);
+        scanf("%d", &fruta);
+        if (fruta < 0){
+            printf("Por acaso voce esta tentando roubar frutas de nosso estoque?");
+            return 1;
+        }
+        frutas[i] = fruta;
+
+    }
+
+    getchar();
+
+    printf("Endereco de entrega: ");
+    fgets(endereco, sizeof(endereco), stdin);
+    printf("Area:");
+    scanf("%c", &area);
+
+    preco = calculaPreco(frutas[0], frutas[1], frutas[2]);
+
+    return 0;
+}
+
+int calculaPreco(int laranja, int maca, int limao){
+    int preco = 0;
+
+    preco = laranja * 3;
+    preco = preco + (preco*3)/100;
+
+
+}
+
+
